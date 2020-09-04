74ff438753f06ebb075b7d99073fdf1b3c8c46ff
Jeremy Felix D <me@jeremyfelix.com>
2020-09-04 07:08:33 -0700

    Added canvas and window functionality

--- main.js
+++ main.js
@@ -1,30 +1,106 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, screen, ipcMain } = require('electron')
 const path = require('path')
 
-function createWindow () {
+let mainWindow, maxWindow;
+function createWindow(windowDisplay) {
+  const WINDOW_WIDTH = 620;
+  const WINDOW_HEIGHT = 744;
+  let displayBounds = windowDisplay.bounds;
+  let windowX = displayBounds.x + ((displayBounds.width - WINDOW_WIDTH) / 2);
+  let windowY = displayBounds.y + ((displayBounds.height - WINDOW_HEIGHT) / 2);
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
+    x: windowX,
+    y: windowY,
+    useContentSize: true,
+    minHeight: 300,
+    minWidth: 300,
+    autoHideMenuBar: true,
+    frame: true,
+    show: false,
+    alwaysOnTop: true,
+    //backgroundColor: '#808080',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  console.log(process.argv0)
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //Show the window after it's finished loading
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    // Avoid window creation on bottom when created on blur (sorta a workaround I guess but it works)
+    mainWindow.setAlwaysOnTop(false);
+  })
+
+  //Need to close the window on maximize and create a new frameless window
+  mainWindow.on('maximize', function () {
+    let currentDisplay = screen.getDisplayMatching(mainWindow.getBounds());
+    mainWindow.hide();
+    createMaxWindow(currentDisplay);
+    mainWindow.close();
+  })
+}
+
+function createMaxWindow(windowDisplay) {
+  // Create the browser window.
+  maxWindow = new BrowserWindow({
+    width: windowDisplay.workAreaSize.width,
+    height: windowDisplay.workAreaSize.height,
+    minWidth: windowDisplay.workAreaSize.width,
+    minHeight: windowDisplay.workAreaSize.height,
+    x: windowDisplay.bounds.x,
+    y: windowDisplay.bounds.y,
+    autoHideMenuBar: true,
+    frame: false,
+    transparent: true,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+
+  // and load the index.html of the app.
+  maxWindow.loadFile('index.html')
+
+  //Show the window after it's finished loading
+  maxWindow.once('ready-to-show', () => {
+    maxWindow.show()
+  })
+
+  // Hide the window and create a framed window
+  maxWindow.changeUnmax = function(){
+    if(maxWindow.isVisible){
+      let currentDisplay = screen.getDisplayMatching(maxWindow.getBounds());
+      maxWindow.hide();
+      createWindow(currentDisplay);
+      maxWindow.close();
+    }
+  }
+
+  // Hide/destroy the window on blur
+  maxWindow.on('blur', function(){
+    maxWindow.changeUnmax()
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  
+  //Create Initial window in the display containing the cursor
+  createWindow(screen.getDisplayNearestPoint(screen.getCursorScreenPoint()))
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -39,5 +115,14 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+// ********************************************************************************
+// Main Process Code
+// ********************************************************************************
+
+// Catch unmaximize event from renderer and unmax the window
+ipcMain.on('click-window-unmaximize', function (event, arg) {
+  if (maxWindow.isVisible()) {
+    maxWindow.changeUnmax()
+  }
+});
--- renderer.js
+++ renderer.js
@@ -4,3 +4,79 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const { ipcRenderer } = require('electron');
+
+//Code I was playing with to open files from input args
+/* const { remote } = require('electron')
+const cwd = remote.process.cwd();
+const inputArgs = remote.process.argv;
+const inputPath = inputArgs.pop();
+console.log('cwd: ' + cwd);
+console.log('inputArgs: ' + inputArgs);
+console.log('inputPath: ' + inputPath); */
+
+// Get the #imageCanvas and #imageView elements
+const body = document.getElementById('body');
+const imageCanvas = document.getElementById('imageCanvas');
+const imageView = document.getElementById('imageView');
+
+/**
+ * Calculates and resizes #ImageCanvas and #ImageView elements.
+ * Called on window 'resize' event
+ */
+function onWindowResize(){
+  // Calculate aspect ratio of available area (screen size minus taskbar)
+  // NOTE: This gets the aspect ratio of the primary monitor's available area
+  let aspectRatio = screen.availWidth / screen.availHeight;
+
+  // Resize the canvas to fit the current window according to the calculated aspect ratio
+  // Compares aspect ratio of window vs screen aspect ratio
+  let canvasWidth = ((window.innerWidth / window.innerHeight) < aspectRatio) ? window.innerWidth : window.innerHeight * aspectRatio;
+  let canvasHeight = ((window.innerWidth / window.innerHeight) < aspectRatio) ?  window.innerWidth / aspectRatio : window.innerHeight;
+  imageCanvas.style.width = canvasWidth + 'px';
+  imageCanvas.style.height = canvasHeight + 'px';
+  
+  /*
+  * 
+  * CHANGE HOW THIS LOGIC WORKS
+  * Notice how the image behaves if already zoomed past 1 and then resized
+  * 
+  */
+   //Calculate size of image and set zoom
+  let imageWidth = parseInt(getComputedStyle(imageView).getPropertyValue('--image-width'), 10);
+  let imageHeight = parseInt(getComputedStyle(imageView).getPropertyValue('--image-height'), 10);
+  console.log(`imageWidth: ${imageWidth} imageHeight: ${imageHeight}`);
+  console.log(`window.innerWidth: ${window.innerWidth} window.innerHeight: ${window.innerHeight}`);
+  let widthZoom, heightZoom = 1;
+  if(window.innerWidth < imageWidth){
+    widthZoom = window.innerWidth/imageWidth;
+  }
+  if(window.innerHeight < imageHeight){
+    heightZoom = window.innerHeight/imageHeight;
+  }
+  //Set smallest of the two zoom values
+  let zoom = (widthZoom < heightZoom) ? widthZoom : heightZoom;
+  console.log(`widthZoom: ${widthZoom} heightZoom: ${heightZoom} zoom: ${zoom}`);
+  imageView.style.setProperty('--image-zoom', zoom);
+}
+onWindowResize(); //Call on startup
+
+//Attach window resize event listener
+window.addEventListener('resize', function(e){
+  e.preventDefault();
+  onWindowResize();
+})
+
+// Code to not show image until document is ready
+/*document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log('document ready, displaying image');
+  imageView.style.display = 'block';
+});*/
+
+body.addEventListener("click", function (e) {
+  if (e.composedPath()[0] === this || e.composedPath()[0] === imageCanvas) {
+    console.log('Clicked on body or canvas');
+    ipcRenderer.send('click-window-unmaximize');
+}
+});

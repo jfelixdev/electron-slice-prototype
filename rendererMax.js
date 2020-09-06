6d50cb08b56c81f947b407813834a851c5a543e2
Jeremy Felix D <me@jeremyfelix.com>
2020-09-06 06:21:15 -0700

    Set initial canvas size to available screen size

--- rendererMax.js
+++ rendererMax.js
@@ -5,12 +5,14 @@ const body = document.getElementById('body');
 const imageCanvas = document.getElementById('imageCanvas');
 //const imageView = document.getElementById('imageView');
 
+//Set the canvas size to the available screen area
+imageCanvas.style.width = screen.availWidth + 'px';
+imageCanvas.style.height = screen.availHeight + 'px';
+
 // Add event listener to exit out of maximize on click outside image
 // @todo Set #imageCanvas size on window creation to make this code work properly again
-window.addEventListener("click", function (e) {
-  if (e.composedPath()[0] === this || e.composedPath()[0] === imageCanvas) {
-    console.log('Clicked on body or canvas');
-    console.log(e.composedPath());
+body.addEventListener("click", function (e) {
+  if (e.composedPath()[0] === body || e.composedPath()[0] === imageCanvas) {
     ipcRenderer.send('click-window-unmaximize');
   }
 });
